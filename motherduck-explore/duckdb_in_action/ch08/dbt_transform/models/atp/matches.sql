974a62d2e5419248b3af804ebd96ad10fa62ac62
Andrew Enfield <aenfield@gmail.com>
2025-09-24 15:54:58 -0700

    Starting out w/ simple dbt project - at this point, loading from a .csv file on the web, transforming via simple SQL, and outputting to parquest.

---
+++ motherduck-explore/duckdb_in_action/ch08/dbt_transform/models/atp/matches.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized='external',
+    location='output/matches.parquet',
+    format='parquet'
+)}}
+
+WITH noWinLoss AS (
+    SELECT COLUMNS(col -> 
+        NOT regexp_matches(col, 'w_.*') AND
+        NOT regexp_matches(col, 'l_.*')
+    )
+    FROM {{ source('github', 'matches_file')}}
+)
+
+SELECT * REPLACE (
+    CAST(STRPTIME(tourney_date::VARCHAR, '%Y%m%d') AS date) AS tourney_date
+)
+FROM noWinLoss
